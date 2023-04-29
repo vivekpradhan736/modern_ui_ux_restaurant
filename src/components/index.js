fad1e41485a2c03e606ebe1a1e4e95d46cc62932
Vivek <vivekpradhan736@gmail.com>
2023-04-30 02:07:55 +0530

    MenuItem updated

--- src/components/index.js
+++ src/components/index.js
@@ -1,7 +1,7 @@
 import SubHeading from './SubHeading/SubHeading';
 import Newsletter from './Footer/Newsletter';
 import FooterOverlay from './Footer/FooterOverlay';
-import MenuItem from './Menuitem/index';
+import MenuItem from './MenuItem';
 import Navbar from './Navbar/Navbar';
 
 export {
